38f1bcec1f54619b8a14382c57984b5877e963da
Marlen Brunner <klondikemarlen@gmail.com>
2020-03-15 16:54:20 -0700

    :art: Re-order app initializers to make import pipeline more clear.

--- app/config/initializers/__init__.py
+++ app/config/initializers/__init__.py
@@ -1,6 +1,11 @@
-from app.config.initializers.game import *
+# Import external libraries
 from app.config.initializers.direct import *
 from app.config.initializers.panda3d import *
+
+# Create the most basic game instance
+from app.config.initializers.game import *
+
+# Modify to the game instance to add features and set settings.
 from app.config.initializers.lighting import *
 from app.config.initializers.mouse import *
 from app.config.initializers.key_mapper import *
