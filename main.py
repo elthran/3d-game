4d9a9788042a89502e7819260d46e94288c05621
Marlen Brunner <klondikemarlen@gmail.com>
2020-03-15 18:55:18 -0700

    :recycle: Convert last of original Game code to initializer style.

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 from app import *
 
 
-class Game(ShowBase):
-    def __init__(self):
-        ShowBase.__init__(self)
+class FinalTweaks:
+    def __init__(self, app):
+        app.hero = Hero()
 
-        # properties = WindowProperties()
-        # properties.setSize(1000, 750)
-        # self.win.requestProperties(properties)
+        app.display_damage = OnscreenText(text=f'Damage taken: {5 - app.hero.health}', pos=(1, -0.9), scale=0.07)
+
+        app.training_dummy_monster = TrainingDummyMonster(Vec3(5, 0, 0))
+
+        app.crate_interactive = CrateInteractive(Vec3(-2, 3, 0))
+
+        app.sliding_crate_monster = SlidingCrateMonster(Vec3(2, 7, 0))
 
 
-        self.hero = Hero()
-
-        self.display_damage = OnscreenText(text=f'Damage taken: {5 - self.hero.health}', pos=(1, -0.9), scale=0.07)
-
-        self.training_dummy_monster = TrainingDummyMonster(Vec3(5, 0, 0))
-
-        self.crate_interactive = CrateInteractive(Vec3(-2, 3, 0))
-
-        self.sliding_crate_monster = SlidingCrateMonster(Vec3(2, 7, 0))
 
 
 if __name__ in ['__main__', 'main']:
-    # game = Game()
+    FinalTweaks(game)
     game.run()
