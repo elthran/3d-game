226fa74a25da07a039e127c7e84a32b91b38ab17
Marlen Brunner <klondikemarlen@gmail.com>
2020-04-18 21:21:01 -0700

    :art: Allow platform specific builds.
    On windows do `python setup.py build_apps`
    
    On linux do `bin/dev build`

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         'build_apps': {
             'console_apps': {'launch_game': 'main.py'},
             'platforms': [
-                'manylinux1_x86_64',
+                # 'manylinux1_x86_64',
                 'win_amd64',
                 # 'macosx_10_6_x86_64'
             ],
